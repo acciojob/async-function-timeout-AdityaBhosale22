68129e5f016c6292502aa7fd8732305166a1fcc3
Aditya Bhosale <147322292+AdityaBhosale22@users.noreply.github.com>
2026-01-29 15:29:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const textInput = document.getElementById("text");
+const numberInput = document.getElementById("delay");
+const btnInput = document.getElementById("btn");
+const output = document.getElementById("output");
+
+btnInput.addEventListener("click", async function(e) {
+    const text = textInput.value.trim();
+    const delay = Number(numberInput.value.trim()); 
+
+    e.preventDefault(); 
+
+    function applyDelay(duration) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, duration);
+        });
+    }
+
+    await applyDelay(delay);
+
+    output.innerText = text; 
+});
